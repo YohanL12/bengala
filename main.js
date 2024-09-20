5e264c41763c68fb13a339341bdb38cf7ae75128
YohanL12 <132920112+YohanL12@users.noreply.github.com>
2024-09-20 11:53:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const botões = document.querySelectorAll('.parametro-senha__botao');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhosenha = 12;
+numeroSenha.textContent = tamanhosenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
